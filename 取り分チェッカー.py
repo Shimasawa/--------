ddd176998b3e6b9ccd5b841793d570572d065799
shimasawa <honnomushi@rakuten.jp>
2022-03-03 11:36:05 +0900

    具材追加の設定まで

--- 取り分チェッカー.py
+++ 取り分チェッカー.py
@@ -23,29 +23,31 @@ class Window(tk.Frame):
     def guzai_setting(self):
         self.reset(self.start_menu)
         
-        guzai = tk.StringVar()
-        
         self.setting_frame = tk.Frame(self)
         self.setting_frame.pack()
     
         label = tk.Label(self.setting_frame,text="具材を入力してください")
         label.pack()
 
-        self.entrybox = tk.Entry(self.setting_frame,textvariable=guzai)
+        self.t = tk.StringVar()
+
+        self.entrybox = tk.Entry(self.setting_frame,textvariable=self.t)
+        self.entrybox.bind("<Return>",lambda event:self.add_guzai())
         self.entrybox.pack()
 
-        add_button = tk.Button(self.setting_frame,text="具材追加",command=lambda:self.add_list(guzai))
+        add_button = tk.Button(self.setting_frame,text="具材追加",command=lambda:self.add_guzai())
         add_button.pack()
 
         decide_button = tk.Button(self.setting_frame,text="具材の確定",command=lambda:print("確定"))
         decide_button.pack()
 
-        self.guzai_list = tk.Listbox(self)
+        self.guzai_list = tk.Listbox(self.setting_frame)
+        self.guzai_list.bind("<<ListboxSelect>>",lambda event:self.guzai_list.delete(self.guzai_list.curselection()))
         self.guzai_list.pack()
         
-    def add_list(self,data):
-        self.guzai.set(data)
-        self.guzai_list.insert(data)
+    def add_guzai(self):
+        self.guzai_list.insert(tk.END,self.entrybox.get())
+        self.t.set("")
 
     def reset(self,frame):
         widgets = frame.winfo_children()
